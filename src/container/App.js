f13f58fc075e25d3a855b4d7e53a5b55816b89ba
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-04-08 02:05:29 +0430

    Style: changed noscript message style

--- src/container/App.js
+++ src/container/App.js
@@ -4,46 +4,45 @@ import KeyPad from '../components/KeyPad';
 import './App.css';
 
 const Calculator = () => {
-	const [result, setResult] = useState('');
-	const calculate = () => {
-		var checkResult = '';
-		if (result.includes('--')) {
-			checkResult = result.replace('--', '+');
-		} else {
-			checkResult = result;
-		}
+  const [result, setResult] = useState('');
+  const calculate = () => {
+    let checkResult = '';
+    if (result.includes('--')) {
+      checkResult = result.replace('--', '+');
+    } else {
+      checkResult = result;
+    }
 
-		try {
-			setResult((eval(checkResult) || '') + '');
-		} catch (e) {
-			setResult('error');
-		}
-	};
+    try {
+      setResult((eval(checkResult) || '') + '');
+    } catch (e) {
+      setResult('error');
+    }
+  };
 
-	const onClick = button => {
-		switch (button) {
-			case '=':
-				calculate();
-				break;
-			case 'C':
-				reset();
-				break;
-			default:
-				
-				setResult(result + button);
-				break;
-		}
-	};
-	const reset = () => {
-		setResult('');
-	};
+  const onClick = button => {
+    switch (button) {
+      case '=':
+        calculate();
+        break;
+      case 'C':
+        reset();
+        break;
+      default:
+        setResult(result + button);
+        break;
+    }
+  };
+  const reset = () => {
+    setResult('');
+  };
 
-	return (
-		<main className='calculator-wrapper'>
-			<Result result={result} />
-			<KeyPad onClick={onClick} />
-		</main>
-	);
+  return (
+    <main className='calculator-wrapper'>
+      <Result result={result} />
+      <KeyPad onClick={onClick} />
+    </main>
+  );
 };
 
 export default Calculator;
